daff1514345e4a1ea1bd71dc6d380460a7a55928
Leo <lvm9508@gmail.com>
2025-06-19 15:55:53 -0300

    Mestre - Desafio finalizado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,25 +1,150 @@
 #include <stdio.h>
+int tabuleiro[10][10] = {0}; // inicia a matriz 10 x 10.
+int error = 0; // variavel pra controle e gerenciamento de error.
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+// Novato
+void desenharLinha() {
+    int centro = 0;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            int coluna = j;
+            int linha = centro;
+            if(linha < 0 || linha >= 10 || coluna < 0 || coluna >= 10) {
+                printf("Posição invalida!");
+                printf("\n");
+                error = 1;
+                break;
+            }
+            tabuleiro[linha][coluna] = 3;
+        }
+        if(error) break;
+    }
+}
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-    int tabuleiro[10][10] = {0};
-    int LINHAS = 9; 
-    int COLUNAS = 1;
-    int i, j;
-    char letras[1][10]; // Uma linha, quatro colunas
-    int col = 0;
+void desenharColuna() {
+    int centro = 0;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 1; j++) {
+            int coluna = centro ;
+            int linha = i ;
+            if(linha < 0 || linha >= 10 || coluna < 0 || coluna >= 10) {
+                printf("Posição invalida!");
+                printf("\n");
+                error = 1;
+                break;
+            }
+            tabuleiro[linha][coluna] = 3;
+        }
+        if(error) break;
+    }
+}
+
+// Aventureiro
+void desenharDiagonal() {
+    // Diagonal para esquerda
+    for(int i = 0; i < 3; i++) {
+        int centro = 0;
+        for(int j = 0; j < 3; j++) {
+            int coluna = centro + j;
+            int linha = j + 1;
+            if(linha < 0 || linha >= 10 || coluna < 0 || coluna >= 10) {
+                printf("Posição invalida!");
+                printf("\n");
+                error = 1;
+                break;
+            }
+            tabuleiro[linha][coluna] = 3;
+        }
+        if(error) break;
+    }
+
+    // Diagonal para direita
+    for(int i = 0; i < 3; i++) {
+       int centro = 3; // controla em qual coluna ficara o centro do navio;
+        for(int j = -1; j <= 1; j++) {
+            int coluna = centro - j;
+            int linha = j + 1; // controla a linha onde começará o navio.
+            // if para controlar as bordar do tabuleiro, caso o valor ultrapasse as bordar será retornado um erro.
+            if(linha < 0 || linha >= 10 || coluna < 0 || coluna >= 10) {
+                printf("Posição invalida!");
+                printf("\n");
+                error = 1;
+                break;
+            }
+            tabuleiro[linha][coluna] = 3; // atualiza o tabuleiro com o navio marcado.
+        }
+        if(error) break;
+    }
     
-    // Definicão do index das colunas.
-    printf("  ");
-    for (i = 0; i < 10; i++) {
-        for(j = 0; j < 10; j++) {
+}
+
+// Mestre
+void desenharCone() {
+    int centro = 7; // controla a coluna baseado no centro.
+    for(int i = 0; i < 3; i++) {
+        for(int j = -i; j <= i; j++) {
+            int coluna = centro + j;
+            int linha = i; // controla a linha de inicio.
+            // verifica se o elemento esta dentro do tabuleiro, se não estiver, dispara um error e encerra o loop interno.
+            if(linha < 0 || linha >= 10 || coluna < 0 || coluna >= 10 || centro < 2 || centro > 7 ) {
+                printf("Posição invalida!");
+                printf("\n");
+                error = 1;
+                break;
+            }
+            tabuleiro[linha][coluna] = 3;
+        }
+        if(error) break; // encerra o loop externo em caso de erro.
+    }
+}
+
+void desenharOcta() {
+    int centro = 1;
+    for(int i = 0; i < 3; i++) {
+        for(int j = -i; j <= i; j++) {
+            int coluna = centro + j;
+            int linha = i ;
+            if(linha < 0 || linha >= 10 || coluna < 0 || coluna >= 10 || centro < 2 || centro > 7 ) {
+                printf("Posição invalida!");
+                printf("\n");
+                error = 1;
+                break;
+            }
+            tabuleiro[linha][coluna] = 2;
+            tabuleiro[4 - linha][coluna] = 2;
+        }
+        if(error) break;
+    }
+}
+
+void desenharCruz() {
+    int centro = 0;
+    for(int i = 0; i < 3; i++) {
+        int linha = centro + 2;
+        int coluna = centro + 2;
+        for(int j = 0; j < 3; j++){
+            if(linha < 0 || linha >= 10 || coluna < 0 || coluna >= 10) {
+                printf("Posição invalida!");
+                printf("\n");
+                error = 1;
+                break;
+            }
+            tabuleiro[linha][coluna] = 1; // exibe o valor 1 no centro da cruz.
+            tabuleiro[linha - 1][coluna] = 1; // valor 1 acima do centro.
+            tabuleiro[linha + 1][coluna] = 1; // valor 1 abaixo do centro.
+            tabuleiro[linha][coluna - 1] = 1; // valor 1 a esquerda do centro.
+            tabuleiro[linha][coluna + 1] = 1; // valor 1 a direita do centro.
+        }
+        if(error) break;
+    }
+}
+
+void desenharTabuleiro() {
+    char letras[1][10]; // Uma linha, quatro colunas
+
+    printf("    ");
+    for (int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
             letras[0][j] = 'A'; // inicializa a matriz com o valor 'A'.
         }
     }
@@ -29,66 +154,34 @@ int main() {
             printf(" %c", letras[0][j]); // printa a matriz com o valo atualizado.
         }
     }
-    
-    for( i = 0; i < 10; i++) {
-        printf("\n%d", i + 1); // define o index da linha.
-        if(i < 9) {
-            printf(" "); // adiciona um espaço a mais nas linhas( excluindo a décima linha ). Para organização do mapa.  
-        } 
-        // controla em qual coluna ira começar e terminar, tambem define o tamanho do navio.
-        for(j = 2; j < 5; j++) {
-            tabuleiro[COLUNAS][j] = 3; //define como o navio deve ser visualizado.
-        }
-        // controla em qual linha ira começar e terminar, tambem define o tamanho do navio.
-        for(j = 0; j < 3; j++) {
-            tabuleiro[j][LINHAS] = 3; // define como o navio deve ser visualizado.
-        }
-       
-        for(j = 5; j < 8; j++) {
-            tabuleiro[j][j] = 3;
-        }
-        for(j = 3; j < 6; j++) {
-            tabuleiro[j][9 - j] = 3;
-        }
-        for(j = 0; j < 10; j++) {
-            printf(" %d", tabuleiro[i][j]); // printa o mapa( matriz 10 x 10 ) com os valores de i e j
-        }
 
+    printf("\n");
+
+    for(int i = 0; i < 10; i++) {
+        printf(" ");
+        printf("%d | ", i );
+        
+        for(int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
     }
-    printf("\n");    
+}
 
-
-
-
-
-
-
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+int main() {
+    // Desafio Novato
+    desenharLinha();
+    desenharColuna();
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    // Desafio Aventureiro
+    desenharDiagonal();
+    
+    // Desafio Mestre
+    desenharCone();
+    desenharOcta();
+    desenharCruz();
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
+    desenharTabuleiro();
+    printf("\n");    
     return 0;
 }
